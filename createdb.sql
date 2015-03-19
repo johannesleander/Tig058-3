82a353ffa49078063ecd1f96540862719829f88c
Philip Crockett <philipmayoo@gmail.com>
2015-03-19 19:01:03 +0100

    Fixed typo

--- createdb.sql
+++ createdb.sql
@@ -47,7 +47,7 @@ insert into team values('F00');
 insert into team values('P00');
 insert into team values('F99');
 insert into team values('P99');
-insert into team values('F98')
+insert into team values('F98');
 insert into team values('P98');
 
 insert into person values ('030301-4125', 'Karin', 'Ungdotter', 'mail@club.se', 1, 0, 0, 1);
@@ -64,5 +64,5 @@ insert into team_roles values ('930201-2575', 'D', 0);
 
 insert into parent_child values ('880301-4145', '030301-4125');
 insert into parent_child values ('880301-4145', '031201-4165');
-insert into parent_child values ('879391-4125', '030301-4125');
-insert into parent_child values ('879391-4125', '031201-4165');
+insert into parent_child values ('870301-4125', '030301-4125');
+insert into parent_child values ('870301-4125', '031201-4165');
