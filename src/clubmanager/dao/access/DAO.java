8ff618c36c519b502b212078ffd4f0088bb459e6
Philip <philipmayoo@gmail.com>
2015-03-19 15:12:23 +0100

    Fixed rename imports and values

--- src/clubmanager/dao/access/DAO.java
+++ src/clubmanager/dao/access/DAO.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package clubmanager.access;
+package clubmanager.dao.access;
 
-import clubmanager.domain.FullPerson;
-import clubmanager.domain.Person;
-import clubmanager.domain.PersonFunction;
+import clubmanager.dao.domain.FullPerson;
+import clubmanager.dao.domain.Person;
+import clubmanager.dao.domain.PersonFunction;
 import java.util.List;
 
 /**
