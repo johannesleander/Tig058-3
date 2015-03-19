d463d0f84f4132820e8c5e6f820a079ed2a93de0
Philip <philipmayoo@gmail.com>
2015-03-19 15:44:09 +0100

    Added getters and setters and constructor to PersonFunction entity

--- src/clubmanager/dao/domain/PersonFunction.java
+++ src/clubmanager/dao/domain/PersonFunction.java
@@ -5,10 +5,36 @@
  */
 package clubmanager.dao.domain;
 
+import java.util.List;
+
 /**
  *
  * @author phcr
  */
-public class PersonFunction {
+public class PersonFunction extends Person {
     
+    private List<String> teams;
+    private int[] roles;
+
+    public PersonFunction() {
+        super();        
+    }    
+    
+    
+    public List<String> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<String> teams) {
+        this.teams = teams;
+    }
+
+    public int[] getRoles() {
+        return roles;
+    }
+
+    public void setRoles(int[] roles) {
+        this.roles = roles;
+    }
+                
 }
