d90f21e95ebd23b566d1ed2a764555fcb997230e
Philip Crockett <philipmayoo@gmail.com>
2015-03-19 18:57:11 +0100

    Added test data to db

--- createdb.sql
+++ createdb.sql
@@ -30,3 +30,39 @@ create table parent_child (
 		   FOREIGN KEY (cid) REFERENCES person(id) on delete cascade on update cascade);
 
 
+
+insert into team values('D');
+insert into team values('H');
+insert into team values('F05');
+insert into team values('P05');
+insert into team values('F04');
+insert into team values('P04');
+insert into team values('F03');
+insert into team values('P03');
+insert into team values('F02');
+insert into team values('P02');
+insert into team values('F01');
+insert into team values('P01');
+insert into team values('F00');
+insert into team values('P00');
+insert into team values('F99');
+insert into team values('P99');
+insert into team values('F98')
+insert into team values('P98');
+
+insert into person values ('030301-4125', 'Karin', 'Ungdotter', 'mail@club.se', 1, 0, 0, 1);
+insert into person values ('031201-4165', 'Kalle', 'Kalleson', 'mail@club.se', 0, 0, 0, 1);
+insert into person values ('880301-4145', 'Kalle', 'Carlsson', 'mail@club.se', 0, 0, 0, 1);
+insert into person values ('870301-4125', 'Ung', 'Carlsson', 'mail@club.se', 1, 0, 0, 1);
+insert into person values ('930201-2575', 'Karin', 'Sportsson', 'mail@club.se', 1, 0, 0, 1);
+
+insert into team_roles values ('030301-4125', 'F03', 0);
+insert into team_roles values ('031201-4165', 'P03', 0);
+insert into team_roles values ('880301-4145', NULL, 1);
+insert into team_roles values ('870301-4125', NULL, 1);
+insert into team_roles values ('930201-2575', 'D', 0);
+
+insert into parent_child values ('880301-4145', '030301-4125');
+insert into parent_child values ('880301-4145', '031201-4165');
+insert into parent_child values ('879391-4125', '030301-4125');
+insert into parent_child values ('879391-4125', '031201-4165');
