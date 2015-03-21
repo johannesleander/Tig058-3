f979311969785ba2004d27945f71e38b0873a1f5
Philip Crockett <philipmayoo@gmail.com>
2015-03-21 14:49:09 +0100

    Added table controller to update

--- src/clubmanager/gui/controller/UpdateController.java
+++ src/clubmanager/gui/controller/UpdateController.java
@@ -1,6 +1,7 @@
 package clubmanager.gui.controller;
 
 import clubmanager.dao.domain.Member;
+import clubmanager.gui.model.MemberTableModel;
 import clubmanager.gui.view.UpdateView;
 
 /**
@@ -12,9 +13,14 @@ public class UpdateController {
     private Member model;
     private UpdateView view;
     private final MainController controller;
+    private MemberTableController mtc;
 
     public UpdateController(MainController controller) {
         this.controller = controller;
+        
+        this.mtc = new MemberTableController(this);
+        this.mtc.setModel(new MemberTableModel());
+        this.mtc.setView(this.view.memberTable);
     }
 
     public void setModel(Member model) {
@@ -24,5 +30,9 @@ public class UpdateController {
     public void setView(UpdateView view) {
         this.view = view;
     }        
+    
+    public void memberSelected(Member m) {
+        this.view.fillViewWithMember(m);
+    }
     
 }
