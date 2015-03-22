19a8a5893d3c8781a6cc89c2d5f71478fc05b587
Philip Crockett <philipmayoo@gmail.com>
2015-03-22 17:48:32 +0100

    Added foreign key support

--- src/clubmanager/ClubManager.java
+++ src/clubmanager/ClubManager.java
@@ -9,6 +9,8 @@ import clubmanager.dao.impl.DAOImpl;
 import clubmanager.gui.controller.MainController;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import org.sqlite.SQLiteConfig;
 
 /**
  *
@@ -22,7 +24,9 @@ public class ClubManager {
      */
     public static void main(String[] args) {
         try {
-            DAOImpl db = new DAOImpl(DriverManager.getConnection("jdbc:sqlite:club.db"));            
+            SQLiteConfig config = new SQLiteConfig();  
+            config.enforceForeignKeys(true);  
+            DAOImpl db = new DAOImpl(DriverManager.getConnection("jdbc:sqlite:club.db", config.toProperties()));            
             MainController mc = new MainController();
             mc.runApp(db);
         } catch (SQLException e) {
