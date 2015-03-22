682f7f5b911840a115fd0e761f32aa1237e574f2
Philip Crockett <philipmayoo@gmail.com>
2015-03-22 19:36:14 +0100

    Javadoc

--- src/clubmanager/domain/comparator/MemberSurnameComparator.java
+++ src/clubmanager/domain/comparator/MemberSurnameComparator.java
@@ -4,12 +4,17 @@ import clubmanager.dao.domain.Member;
 import java.util.Comparator;
 
 /**
- *
- * @author phcr
  * This comparator compares the Surname of the Members, if they are equal it will compare the first name.
+ * @author phcr
  */
 public class MemberSurnameComparator implements Comparator<Member> {
 
+    /**
+     * Compares two members objects surnames, if they have the same surname it compares the firstname.
+     * @param m1
+     * @param m2
+     * @return 
+     */
     @Override
     public int compare(Member m1, Member m2) {
         if (m1.getSurname().equals(m2.getSurname())) {
