bdbd2f577659c0b515242e549de51cb01798e349
Philip Crockett <philipmayoo@gmail.com>
2015-03-23 15:40:07 +0100

    Created the dist proj

---
+++ utils/csvreader.py
@@ -0,0 +1,13 @@
+import csv
+
+
+def read(file, callback):
+    with open(file, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        callback([row for row in reader])
+
+
+def write(file, lst):
+    with open(file, 'w') as f:
+        for (x in lst):
+            f.write(x)
---
+++ utils/format.py
@@ -0,0 +1,36 @@
+import csvreader
+
+header = {
+    'name' : 0,
+    'surname' : 1,
+    'email' : 2,
+    'gender' : 3,
+    'birthdate' : 4,
+    'joindate' : 5,
+    'active' : 6
+    }
+
+personList = {}
+
+person = {
+    id,
+    name,
+    surname.
+    email,
+    gender,
+    birthdate,
+    joindate,
+    active
+    }
+
+
+def formatId(row):
+    
+
+def formatRow(row):
+    
+
+def createCSV():
+    
+if __name__ == '__main__':
+    csvreader.read('person.csv',formatRow)
