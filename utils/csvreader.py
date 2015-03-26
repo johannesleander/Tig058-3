56dcabac0bd92159df3f8e5eb39ca200309553b3
Philip Crockett <philipmayoo@gmail.com>
2015-03-26 18:23:13 +0100

    Created a fromatted CSV for database

--- utils/csvreader.py
+++ utils/csvreader.py
@@ -1,13 +1,14 @@
 import csv
 
-
 def read(file, callback):
     with open(file, 'r') as f:
         reader = csv.DictReader(f, delimiter=',')
         callback([row for row in reader])
 
 
-def write(file, lst):
+def write(file, header, lst):
     with open(file, 'w') as f:
-        for x in lst:
-            f.write(x)
+        writer = csv.DictWriter(f, header, delimiter=',')
+        writer.writeheader()
+        writer.writerows(lst)
+
--- utils/format.py
+++ utils/format.py
@@ -2,6 +2,16 @@ import csvreader
 import datetime
 import random
 
+wheader = ['id','name','surname','email','gender','birthdate','joindate','active']
+wrows = []
+
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
+def unix_time_millis(dt):
+    return unix_time(dt) * 1000.0
 
 def createId(bdate):
     dl = list(map(int, bdate.split('-')))
@@ -9,8 +19,19 @@ def createId(bdate):
     id = dt.strftime('%y%m%d')
     return id + "-" + str(random.randint(1000,9999))
 
-def formatRow(row):
-    print(row)
+def str_dt(s):
+    d = list(map(int, s.split('-')))
+    return datetime.datetime(d[0],d[1],d[2])
+
+
+def formatRows(rows):
+    for row in rows:
+        row['id'] = createId(row['birthdate'])
+        row['birthdate'] = unix_time_millis(str_dt(row['birthdate']))
+        row['joindate'] = unix_time_millis(str_dt(row['joindate']))
+        wrows.append(row)
 
 if __name__ == '__main__':
-    csvreader.read('person.csv',formatRow)
+    csvreader.read('person.csv',formatRows)
+    csvreader.write('formatted.csv', wheader, wrows)
+    
